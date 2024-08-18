1062853e1b23064c293b469d7c1d111726ac352f
Venkatesh081a <vullagantivenkatesh@gmail.com>
2024-08-18 16:14:41 +0530

    Adding the Delete endpoint

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, jsonify, request
 app1 = Flask(__name__)
 
+tasks = []
+
+@app1.route("/deletetask/<Id>", methods=['DELETE'])
+def deletetask(Id):
+    for i in range(len(tasks)):
+        if tasks[i]['id'] == int(Id):
+            del tasks[i]
+            return jsonify(" The task has been deleted")
+        else:
+           return jsonify("Invalid ID !") 
+        
 if __name__== '__main__':
     app1.run(debug=True)
