0570536d914e2b3048e4288d6c47a600fbba381c
Ashraf Khan <ashrafkhan.du@gmail.com>
2024-08-18 15:26:05 +0530

    I updated add part

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 from flask import Flask, jsonify, request
 app1 = Flask(__name__)
 
+tasks = []
+@app1.route("/tasks", methods=['GET'])
+def getAllTasks():
+    return jsonify(tasks)
+
+
+@app1.route("/tasks/<Id>", methods=['GET'])
+def getProductById(Id):
+    for task in tasks:
+        if (task['id'] == int(Id)):
+            return jsonify(task) 
+
+@app1.route("/addtask", methods=['POST'])
+def addtasks():
+    # request and response
+    task = request.json
+    tasks.append(task)
+    return jsonify(" The new tasks has been added")
+
+
 if __name__== '__main__':
     app1.run(debug=True)
